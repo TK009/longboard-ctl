886800113ab4efac40d3597b9925a23e3c44b5be
Kinnunen Tuomas <tuomas.kinnunen@aalto.fi>
2015-10-16 11:45:34 +0300

    add template for the code

---
+++ main.c
@@ -0,0 +1,19 @@
+/**
+ * For an electronic longboard (long skateboard) project.
+ * To control twin electronic speed controller and some on-board electronics.
+ *
+ * Also connects to some android software via bluetooth to show information
+ * about battery etc.
+ */
+
+
+void setup() {
+    // Connect i2c accelerometer
+
+    // Connect bluetooth serial
+}
+
+// MAINLOOP
+void loop() {
+
+}
