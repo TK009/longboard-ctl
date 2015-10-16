3b018ce49f2d3e46401640b35641a98a78d757e7
Kinnunen Tuomas <tuomas.kinnunen@aalto.fi>
2015-10-16 12:22:08 +0300

    added accelerometer library as git submodule
    to clone all: git submodule update --init

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/**
+ * Main controller for an electronic longboard (long skateboard) project.
+ */
+
+
+void setup() {
+    // Connect i2c accelerometer
+    setupAccelerometer();
+
+    // Connect bluetooth serial
+}
+
+// MAINLOOP
+void loop() {
+
+}
+
+void setupAccelerometer() {
+
+}
+
+void setupBluetooth() {
+
+}
+
